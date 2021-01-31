ba12b284a76ad5a788985d07c9f9fc6a0e610bfe
Ankizle <ankizleyt@gmail.com>
2021-01-31 04:25:53 -0500

    added base convert up to base 32

--- test/all_test.go
+++ test/all_test.go
@@ -3,6 +3,7 @@ package tusktest
 //this is the test suite for tools
 
 import (
+	"os"
 	"testing"
 
 	"github.com/tusklang/goat"
@@ -10,11 +11,18 @@ import (
 )
 
 func TestAll(t *testing.T) {
-	lib, e := goat.LoadLibrary("./tusk", types.CliParams{})
+
+	os.Chdir("..") //go to the parent directory (for tuskstd)
+
+	lib, e := goat.LoadLibrary("./test/tusk", types.CliParams{})
 
 	if e != nil {
 		panic(e)
 	}
 
-	goat.CallOatFunc(lib, "main")
+	_, e2 := goat.CallOatFunc(lib, "main")
+
+	if e2 != nil {
+		(*e2).Print()
+	}
 }
